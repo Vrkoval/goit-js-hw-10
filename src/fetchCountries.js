f6626a39d0da22cb674c9e2432ecdc156687909b
Viktoriya Koval <vrkoval@gmail.com>
2023-04-20 00:04:57 +0300

    fetchCountry

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries(name) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    const URL = `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+    return fetch(URL).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json()
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+
+const refs = {
+        searchBox: document.querySelector('#search-box'),
+        listCountries: document.querySelector('.country-list'),
+        countryInfo: document.querySelector('.country-info'),
+    };
+
+
+refs.searchBox.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(event) {
+  
+
+    let searchingCountry = event.target.value.trim();
+    if (!searchingCountry) {
+        resetMarkup(refs.listCountries);
+        resetMarkup(refs.countryInfo);
+        return;
+    };
+
+    fetchCountries(searchingCountry)
+        .then(country => {
+            if (country.length > 10) {
+                 Notify.info("Too many matches found. Please enter a more specific name.");
+            }
+            else if (country.length <= 10 && country.length >= 2) {
+                resetMarkup(refs.listCountries);
+                createMarkupForCountries(country);
+                resetMarkup(refs.countryInfo);
+            }
+            else {
+                resetMarkup(refs.countryInfo);
+                createCardForCountry(country);
+                resetMarkup(refs.listCountries);
+            }
+        })
+        .catch(error => {
+            resetMarkup(refs.listCountries);
+            resetMarkup(refs.countryInfo);
+            Notify.failure("Oops, there is no country with that name");
+        });
+}
+
+function createMarkupForCountries(countriesArray) {
+        const markup = countriesArray
+        .map(({ name, flags }) => {
+            return`<li class="country-item">
+        <img class="country-flag" src="${flags.svg}" alt="flags" width = "50" height = "30">
+        <h2 class="country-name">${name.official}</h2> 
+        </li>`;
+        })
+   .join('');
+   return refs.listCountries.insertAdjacentHTML('beforeend', markup);
+    }
